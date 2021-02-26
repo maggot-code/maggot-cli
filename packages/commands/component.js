097435b313f24ca186d8f97b45224f4a1aa0343d
maggot_code <wowhuanhuan233@163.com>
2021-02-26 18:03:31 +0800

    feat：command add
    新增：组件指令占位
    新增：视图组件占位

---
+++ packages/commands/component.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-02-26 17:48:51
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-02-26 17:49:14
+ * @Description: file content
+ */
+const path = require('path');
+const fs = require('fs');
+
+function ComponentRun(componentName) {
+    console.log(componentName);
+}
+
+module.exports = ComponentRun;
---
+++ packages/commands/view.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-02-26 17:43:46
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-02-26 17:44:06
+ * @Description: file content
+ */
+const path = require('path');
+const fs = require('fs');
+
+function ViewRun(viewName) {
+    console.log(viewName);
+}
+
+module.exports = ViewRun;
