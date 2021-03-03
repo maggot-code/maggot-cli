5022b8b4f096fc12665c3322ca6fd538d30166f9
maggot_code <wowhuanhuan233@163.com>
2021-03-03 14:34:20 +0800

    feat：add view template
    1.加入view模板生成入口文件
    2.加入view模板生成vue文件
    3.加入view模板生成scss文件

--- packages/commands/view.js
+++ packages/commands/view.js
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-02-26 17:43:46
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-02-26 17:44:06
+ * @LastEditTime: 2021-03-03 14:32:22
  * @Description: file content
  */
 const path = require('path');
---
+++ packages/module/view/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-03-03 14:28:15
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-03-03 14:29:29
+ * @Description: file content
+ */
+import Template from './template.vue';
+
+export default Template;
